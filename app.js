f50e9afd835264d36e2347b64440f3c0eea76b4e
vwital <wital128@tut.by>
2024-01-04 17:02:08 +0300

    refactor(extended-2.3): class names according BEM notation

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ var createNewTaskElement=function(taskString){
     //Each elements, needs appending
     checkBox.type="checkbox";
     editInput.type="text";
-    editInput.className="task task-container__input";
+    editInput.className="task add-item__input";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
     editButton.className="button edit";
